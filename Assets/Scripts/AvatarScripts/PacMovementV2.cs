8e66ff3d4780b81713bdddc61ef0d3665c3b6ce5
Emmanuel747 <EmmanuelEEjim@gmail.com>
2023-04-17 10:35:13 -0500

    Fixed both AI's and

--- Assets/Scripts/AvatarScripts/PacMovementV2.cs
+++ Assets/Scripts/AvatarScripts/PacMovementV2.cs
@@ -1,34 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
+public class PacMovementV2 : MonoBehaviour
+{
+  public float moveForce = 0.8f; // The force applied to the object
+  public float moveSpeed = 7f; // The speed at which Pacman moves
+  public float maxSpeed = 8.9f; // The maximum speed Pacman can reach
+  public float rotateSpeed = 1.5f; // The speed at which Pacman rotates
+  public float jumpForce = 100f; // The force applied to the jump
+  public float fallMultiplier = 50f; // The multiplier applied to the fall speed
+  public float strafeForce = 10f; // The force applied for strafing
 
+  public UnityEngine.AI.NavMeshAgent enemy;
+  public Transform Player;
 
+  private Vector3 initialMousePosition;
+  // private bool mouseRightClickHeld = false;
+  
+  private Rigidbody rb; // The Rigidbody component of Pacman
+  private int jumpCount = 1;
+  public int consumedPellets = 0;
+  public float rotSpeed = 1 ;
 
-  // public bool IsMoving() {
-  //   return rb.velocity != Vector3.zero;  
-  // }
-    // [Work in Progress] Rotate based on mouse right-click drag direction
-    // if (Input.GetMouseButtonDown(1))
-    // {
-    //     mouseRightClickHeld = true;
-    //     initialMousePosition = Input.mousePosition;
-    //     Debug.Log(initialMousePosition);
-    // }
-    // else if (Input.GetMouseButtonUp(1))
-    // {
-    //     mouseRightClickHeld = false;
-    // }
+  private Score scoreScript; // Outside script variable
 
-    // if (mouseRightClickHeld)
-    // {
-    //   Vector3 currentMousePosition = Input.mousePosition;
-    //   float rotationSpeed = (currentMousePosition - initialMousePosition).magnitude * 0.01f;
-    //   // rotateSpeed = rotationSpeed <
-      
-    //   Vector3 rotationDirection = (currentMousePosition.x < initialMousePosition.x) ? -transform.right : transform.right;
-    //   // mouseRotation = rotationDirection.x * rotationSpeed
-    //   Debug.Log(rotationDirection);
-    //   // transform.Rotate(0f, rotationDirection.x * 0.3f, 0f, Space.Self);
-    //   if (rotationDirection.x > 0) {
-    //     transform.Rotate(0f, rotateSpeed, 0f);
-    //   } else transform.Rotate(0f, -rotateSpeed, 0f);
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    scoreScript = GameObject.Find("ScoreContainerCanvas").GetComponent<Score>();
+  }
 
-    // }
+void Update()
+{
+    // Limit the maximum speed
+    if (rb.velocity.magnitude > maxSpeed)
+    {
+        rb.velocity = rb.velocity.normalized * maxSpeed;
+    }
+
+    // Move forwards and backwards based on current rotation
+    Vector3 moveDirection = Vector3.zero;
+    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+    {
+        moveDirection -= transform.right;
+    }
+    else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+    {
+        moveDirection += transform.right;
+    }
+
+    // Rotate left and right on Y axis
+    if (Input.GetKey(KeyCode.A))
+    {
+        transform.Rotate(0f, -rotateSpeed, 0f);
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+        transform.Rotate(0f, rotateSpeed, 0f);
+    }
+
+    // Apply movement force only if W, A, S, D are pressed
+    if (moveDirection != Vector3.zero)
+    {
+        rb.AddForce(moveDirection * moveSpeed * moveForce, ForceMode.VelocityChange);
+    } else if (jumpCount == 0) {
+      rb.velocity = Vector3.zero;
+    }
+
+    // Increase fall speed
+    if (rb.velocity.y < 0)
+    {
+        rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+    }
+
+    // Double jump on press of spacebar
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+        if (jumpCount < 2)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+            jumpCount++;
+        }
+    }
+
+    // Strafe left and right
+    if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+    {
+        rb.AddForce(-transform.forward * strafeForce * moveForce, ForceMode.VelocityChange);
+    }
+    else if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightArrow))
+    {
+        rb.AddForce(transform.forward * strafeForce * moveForce, ForceMode.VelocityChange);
+    }
+
+    // sending tracking data to enemy
+    enemy.SetDestination(Player.position);
+}
+
+  // Reset jump count when Pacman lands on the ground
+  void OnCollisionEnter(Collision collision)
+    {
+      if (collision.gameObject.tag == "Ground")
+      {
+        jumpCount = 0;
+      }
+
+      // HardMode Game Mechanic where hitting walls = -10 score
+      if (collision.gameObject.tag == "wall")
+      {
+        scoreScript.playerScoreDEC(10);
+      }
+    }
+
+  // Consume Pellets upon collision & Update Completion %
+  private void OnTriggerEnter(Collider Other) {
+    if (Other.gameObject.CompareTag("Pellet")) {
+      scoreScript.playerScoreINC(105);
+      Destroy(Other.gameObject);
+      consumedPellets += 1;
+    } else if (Other.gameObject.CompareTag("PowerOrb")) {
+      scoreScript.playerScoreINC(277);
+      Destroy(Other.gameObject);
+    }
+  }
+}
--- Assets/Scripts/Enemies/nonGhostController.cs
+++ Assets/Scripts/Enemies/nonGhostController.cs
@@ -3,59 +3,73 @@ using UnityEngine.SceneManagement;
 using UnityEngine.AI;
 
 public class nonGhostController : MonoBehaviour {
-    // Passed in GameObjects
-    public GameObject pacMan;
-    public Animator animator;
-    public NavMeshAgent navAgent;
+  // Passed in GameObjects
+  public GameObject pacMan;
+  public Animator animator;
+  public NavMeshAgent navAgent;
 
-    // public options for devs to set during testing
-    public float moveSpeed = 14f;
-    public float runSpeedMultiplier = 5f; // add run speed multiplier
-    public float aniScaleSizeFloat = 16;
+  // public options for devs to set during testing
+  public float moveSpeed = 14f;
+  public float runSpeedMultiplier = 5f; // add run speed multiplier
+  public float aniScaleSizeFloat = 16;
 
-    //Dev Bool to stop enemies from ending the game
-    public bool gameOverOn = false;
+  // Dev Bool to stop enemies from ending the game
+  public bool gameOverOn = false;
+  private Score scoreScript;
+  
+  private void Start() {
+      scoreScript = GameObject.Find("ScoreContainerCanvas").GetComponent<Score>();
+      animator = GetComponent<Animator>();
+      navAgent = GetComponent<NavMeshAgent>();
+      navAgent.updateRotation = false;
+      navAgent.enabled = true;
+  }
 
-    private void Start() {
-        animator = GetComponent<Animator>();
-        navAgent = GetComponent<NavMeshAgent>();
-        navAgent.updateRotation = false;
-        navAgent.enabled = true;
+  private void Update() {
+   
+    
+    if (scoreScript.GetCompletionPercentage() > 35f) {
+      navAgent.isStopped = false;
+      Debug.Log("2nd Enemy Released");
+    }
+    else {
+      navAgent.isStopped = true;
     }
 
-    private void Update() {
-        if (pacMan != null) {
-            // move towards pac-man
-            float speed = moveSpeed;
-            if (Vector3.Distance(transform.position, pacMan.transform.position) >= 5f) { // check distance to target
-                speed *= 1.3f; // apply run speed multiplier   
-                animator.SetBool("running", true);
-            } else {
-                animator.SetBool("running", false);
-            }
-            navAgent.speed = speed;
-            navAgent.SetDestination(pacMan.transform.position);
+    if (pacMan != null) {
+      // move towards pac-man
+      float speed = moveSpeed;
+      if (Vector3.Distance(transform.position, pacMan.transform.position) >= 5f) { // check distance to target
+          speed *= 1.3f; // apply run speed multiplier   
+          animator.SetBool("running", true);
+      } else {
+          animator.SetBool("running", false);
+      }
+      navAgent.speed = speed;
+      navAgent.SetDestination(pacMan.transform.position);
 
-            // determine which way to face based on movement direction
-            if (transform.position.x < pacMan.transform.position.x) {
-                transform.localScale = new Vector3(aniScaleSizeFloat, aniScaleSizeFloat, aniScaleSizeFloat); // facing right
-            } else {
-                transform.localScale = new Vector3(-aniScaleSizeFloat, aniScaleSizeFloat, aniScaleSizeFloat); // facing left
-            }
+      // determine which way to face based on movement direction
+      if (transform.position.x < pacMan.transform.position.x) {
+        transform.localScale = new Vector3(aniScaleSizeFloat, aniScaleSizeFloat, aniScaleSizeFloat); // facing right
+        transform.LookAt(pacMan.transform);
+      } else {
+        transform.localScale = new Vector3(-aniScaleSizeFloat, aniScaleSizeFloat, aniScaleSizeFloat); // facing left
+        transform.LookAt(pacMan.transform);
+      }
 
-            // set the speed of the run animation
-            animator.SetFloat("Blend", moveSpeed * runSpeedMultiplier * 0.009f);
-        }
+      // set the speed of the run animation
+      animator.SetFloat("Blend", moveSpeed * runSpeedMultiplier * 0.009f);
     }
+  }
 
-    private void OnCollisionEnter(Collision collision) {
-      if (collision.gameObject.CompareTag("Pacman") && gameOverOn) {
-            SceneManager.LoadScene("GameOver");
-        } else if (collision.gameObject.CompareTag("wall")) {
-            navAgent.ResetPath();
-            Vector3 direction = pacMan.transform.position - transform.position;
-            direction.y = 0;
-            transform.rotation = Quaternion.LookRotation(direction);
-        }
-    }
+  private void OnCollisionEnter(Collision collision) {
+    if (collision.gameObject.CompareTag("Pacman") && gameOverOn) {
+        SceneManager.LoadScene("GameOver");
+      } else if (collision.gameObject.CompareTag("wall")) {
+        navAgent.ResetPath();
+        Vector3 direction = pacMan.transform.position - transform.position;
+        direction.y = 0;
+        transform.rotation = Quaternion.LookRotation(direction);
+      }
+  }
 }
